fcc032d2e7a10638405fa2d4c29cc7b57ad0b4a4
Flor21 <fas-2196@hotmail.com>
2019-09-29 20:23:31 -0300

    Subiendo ejercicio y docker

---
+++ prueba/src/main/java/pruebaUnitaria/prueba/App.java
@@ -0,0 +1,17 @@
+package pruebaUnitaria.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Compara si el primer numero es mayor que el primero. Si no es asi sale un aviso");
+    }
+    
+    public static boolean comparar(Integer A, Integer B) {
+    	return (A>B);
+    }
+}
---
+++ prueba/src/test/java/pruebaUnitaria/prueba/AppTest.java
@@ -0,0 +1,16 @@
+package pruebaUnitaria.prueba;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private static App obj_prueba;
+    
+    @Test
+    public void compararTest() {
+    	assertTrue ("El primer numero es menor que el segundo", obj_prueba.comparar(10, 7));
+    }
+}
